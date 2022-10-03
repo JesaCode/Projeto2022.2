906d981d9480fa40890aaae3d952e849d682d5e7
victormelo87s <vppm@cesar.schoolm>
2022-10-03 20:20:59 -0300

    desenvolvimento do cadastro

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,13 @@
 from pipes import Template
+
 from django.urls import path
 
 from . import views
 
+app_name = 'main'
+
 urlpatterns = [
-    path('cadastro/', views.cadastro),
-    path('', views.index)
+    path('', views.index),
+    path('registro/', views.registro_cadastro, name='registro'),
+    path('registro/criado/', views.registro_criado, name='criado'),
 ]
